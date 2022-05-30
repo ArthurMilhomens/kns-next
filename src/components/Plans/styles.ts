b20255e67412a8e887e5217f4134bdb6078329a4
GabrielMTF <gabrielmota287@gmail.com>
2022-05-30 14:36:22 -0300

    implement of plans page/improvements

--- src/components/Plans/styles.ts
+++ src/components/Plans/styles.ts
@@ -20,59 +20,21 @@ export const ContainerPlans = styled.div`
     margin: 6.25rem;
     text-align: center;
     padding: 3.5rem 4rem 0 4rem;
-`;
 
-export const P = styled.p`
-    font-family: Roboto;
-    font-size: 1.9rem;
-    font-weight: 600;
-    color: var(--white);
-`;
-
-export const Ptittle = styled.p`
-    font-family: Roboto;
-    font-size: 1.6rem;
-    font-weight: 500;
-    color: var(--white);
-    margin-top: 3.5rem;
-    text-align: left;
-`;
-
-export const Plist = styled.p`
-    font-family: Roboto;
-    font-size: 1.6rem;
-    font-weight: 500;
-    color: var(--white);
-    padding: 0.9rem 0 0.2rem 0;
-    text-align: left;
-`;
-
-export const OL = styled.ol`
-    margin-top: 3rem;
-    margin-bottom: 4.5rem;
-    list-style-type: disc !important;
-    padding-left: 2rem;
-
-`;
-
-export const Button = styled.button`
-    width: 20.75rem;
-    height: 3.75rem;
-    background-color: var(--red);
-    color: var(--white);
-    border-radius: 3.75rem;
-    border-style: none;
-    font-size: 1.25rem;
-    text-align: center;
-    justify-content: center;
-    align-items: center;
-    padding: 0.5rem 0 0 0;
-    opacity: 0.7;
-    &:hover {
-        opacity: 1;
-        transition: 0.5s;
+    p {
+        font-family: Roboto;
+        font-size: 1.9rem;
+        font-weight: 600;
+        color: var(--white);
     }
-
+    p.title {
+        font-family: Roboto;
+        font-size: 1.6rem;
+        font-weight: 500;
+        color: var(--white);
+        margin-top: 3.5rem;
+        text-align: left;
+    }
 `;
 
 export const ContainerList = styled.div`
@@ -82,21 +44,45 @@ export const ContainerList = styled.div`
         display: none;
     }
 
+    ol {
+        margin-top: 3rem;
+        margin-bottom: 4.5rem;
+        list-style-type: disc !important;
+        padding-left: 2rem;
+    }
+
+    li {
+        font-family: Roboto;
+        font-size: 1.6rem;
+        font-weight: 500;
+        color: var(--white);
+        padding: 0.9rem 0 0.2rem 0;
+        text-align: left;
+    }
+
 `;
 
 export const ContainerButton = styled.div`
     padding: 0.5rem 0.5rem 0.5rem 0.5rem;
-`;
 
-export const PInfo = styled.p`
-    font-family: Roboto;
-    font-weight: 400;
-    font-size: 1.25rem;
-    color: var(--white);
-`;
-
-export const Span = styled.span`
-    text-decoration: underline;
+    button {
+        width: 20.75rem;
+        height: 3.75rem;
+        background-color: var(--red);
+        color: var(--white);
+        border-radius: 3.75rem;
+        border-style: none;
+        font-size: 1.25rem;
+        text-align: center;
+        justify-content: center;
+        align-items: center;
+        padding: 0.5rem 0 0 0;
+        opacity: 0.7;
+        &:hover {
+            opacity: 1;
+            transition: 0.5s;
+        }
+    }
 `;
 
 export const ContainerInfo = styled.div`
@@ -104,6 +90,16 @@ export const ContainerInfo = styled.div`
     width:100%;
     height:13.5rem;
     color:red;
-    margin-top: 51rem;
+    margin-top: 55rem;
     text-align: center;
+
+    p {
+        font-family: Roboto;
+        font-weight: 400;
+        font-size: 1.25rem;
+        color: var(--white);
+    }
+    span {
+        text-decoration: underline;
+    }
 `;
